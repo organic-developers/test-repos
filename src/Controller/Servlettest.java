eb76506689a8c1a0f4b8a791f3ee2360a36950b7
Amir <amir.ehsani.jam@gmail.com>
2015-08-13 21:08:57 +0430

    new file added

---
+++ src/Controller/Servlettest.java
@@ -0,0 +1,16 @@
+package Controller;
+
+import java.io.IOException;
+
+/**
+ * Created by Amir on 8/13/2015.
+ */
+public class Servlettest extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
